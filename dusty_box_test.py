b07560c381223cec2b2be3da4f9817976b349ec7
gouldenm <gouldenm@tcd.ie>
2019-12-13 03:29:24 +0000

    Dusty box test still acting weird

--- dusty_box_test.py
+++ dusty_box_test.py
@@ -8,30 +8,44 @@ x=np.arange(0,nx)*dx
 t=5.0
 
 def analytical(pd0, pg0, rho_g0, rho_d0, K, t):		#NB pg0 can be used because gas is constant
-	term1 = pd0*np.exp(-K*rho_g0*t) + (rho_d0/rho_g0)*pg0*(1-np.exp(-K*rho_g0*t))
+	sol = pd0*np.exp(-K*rho_g0*t) + (rho_d0/rho_g0)*pg0*(1-np.exp(-K*rho_g0*t))
+	return(sol)
 
 
 #	Run simulations over a range of timesteps
 times = [1e-1, 1, 2, 3, 4, 5]
 K=1.0
-errs=[]
+errs_exp=[]
+errs_bw=[]
 
 for t in times:
 	#	Set up base grid: initially, velocity of gas is 0, dust velocity initially 1
-	fixed = mesh(200, 1.0, mesh_type = "Lagrangian", K =K, CFL = 0.5)
-	fixed.setup(vL=0.0, rhoL=1.0, PL=1.0, vLd=1.0, rhoLd=1.0, IC="LRsplit", boundary="flow", cutoff=2.0)
+	backwards = mesh(200, 1.0, mesh_type = "Lagrangian", K =K, CFL = 0.5)
+	backwards.setup(vL=0.0, rhoL=1.0, PL=1.0, vLd=1.0, rhoLd=1.0, IC="LRsplit", boundary="flow", cutoff=2.0)
 	
-	pd0 = fixed.Q[0,4]
+	pd0 = backwards.Q[0,4]
 	true_pdt = analytical(pd0, 0.0, 1.0, 1.0, K, t)
 	
-	fixed.solve(tend=t, scheme = "approx", plotsep=100)
-	sol_pdt = fixed.Q[0,4]
+	backwards.solve(tend=t, scheme = "approx")
+	bw_pdt = backwards.Q[0,4]
 	
-	err = np.abs(pd0 - sol_pdt)
-	errs.append(err)
+	exp = mesh(200, 1.0, mesh_type = "Lagrangian", K =K, CFL = 0.5)
+	exp.setup(vL=0.0, rhoL=1.0, PL=1.0, vLd=1.0, rhoLd=1.0, IC="LRsplit", boundary="flow", cutoff=2.0)
+	exp.solve(tend=t, scheme = "exp")
+	exp_pdt = exp.Q[0,4]
+	
+	err_exp = np.abs(true_pdt - exp_pdt)
+	errs_exp.append(err_exp)
+	
+	err_bw = np.abs(true_pdt - bw_pdt)
+	errs_bw.append(err_bw)
 
-plt.plot(times, errs)
+plt.figure()
+plt.plot(times, errs_bw, "b-", label="BW error")
+plt.plot(times, errs_exp, "r--", label="Exp error")
 plt.yscale("log")
-print(errs)
+plt.xlabel("time")
+plt.ylabel("absolute error")
+plt.legend()
 
 plt.show()
