0e50b693ef87867b52c2b013280844551c86102f
Maggie Goulden <mcg58@calx044.ast.cam.ac.uk>
2020-02-05 15:14:46 +0000

    Explicit scheme is working to 2nd order in gas, 2nd order for small K in dust

--- working_backwards/dustywave_sol.py
+++ working_backwards/dustywave_sol.py
@@ -1,10 +1,9 @@
-
 import numpy as np
 from scipy.integrate import ode
 
 class _DustyWaveSolution(object):
     def __init__(self, t, k,
-                 rho_g, rho_d, P0, drho_g, drho_d, v_g, v_d, dP, v_f):
+                 rho_g, rho_d, drho_g, drho_d, v_g, v_d):
         self._t = t
         self._k = k
         self._rho_g = rho_g
@@ -13,23 +12,17 @@ class _DustyWaveSolution(object):
         self._drho_d = drho_d
         self._v_g = v_g
         self._v_d = v_d
-        self._P0 = P0
-        self._dP = dP
-        self._v_f = v_f
 
 
     def v_gas(self, x):
-        return (self._v_f + self._v_g * np.exp(1j*self._k*x)).real
+        return (self._v_g * np.exp(1j*self._k*x)).real
     def v_dust(self, x):
-        return (self._v_f + self._v_d * np.exp(1j*self._k*x)).real
+        return (self._v_d * np.exp(1j*self._k*x)).real
 
     def rho_gas(self, x):
         return (self._rho_g + self._drho_g * np.exp(1j*self._k*x)).real
     def rho_dust(self, x):
         return (self._rho_d + self._drho_d * np.exp(1j*self._k*x)).real
-
-    def P(self, x):
-        return (self._P0 + self._dP * np.exp(1j*self._k*x)).real
 
     @property
     def time(self):
@@ -37,12 +30,9 @@ class _DustyWaveSolution(object):
 
 class DustyWaveSolver(object):
 
-    def __init__(self, rho_g=1., rho_d=1., cs=1., K=1., delta=1e-3, vf=0,
-                 GAMMA=1.0, wavelength=1., feedback=True):
-        
-        self.GAMMA = GAMMA
+    def __init__(self, rho_g=1., rho_d=1., cs=1., K=1., delta=1e-3,
+                 wavelength=1., feedback=True):
 
-        self.vf = vf
         self.rho_g = rho_g
         self.rho_d = rho_d
         self.cs = cs
@@ -55,10 +45,8 @@ class DustyWaveSolver(object):
         '''Solve the dusty-wave problem up to specified times'''
         k = 2*np.pi / self.wavelength
         cs2 = self.cs**2
-        gammaP0 = cs2 * self.rho_g 
-        gamma = self.GAMMA
 
-        ts_inv = self.K * self.rho_g
+        ts_inv = self.K * self.cs * self.rho_g
         if self.feedback:
             Kg = (self.rho_d/self.rho_g) * ts_inv
             Kd = ts_inv
@@ -67,24 +55,22 @@ class DustyWaveSolver(object):
             Kd = ts_inv
 
         def f(t, y,_=None):
-            drho_g, v_g = y[0:2]
-            drho_d, v_d = y[2:4]
-            dP = y[4]
+            drho_g, v_g = y[:2]
+            drho_d, v_d = y[2:]
             
-            dydt = np.empty([5], dtype='c8')
-            dydt[0] = - 1j*k*self.vf*drho_g - 1j*k*self.rho_g * v_g 
-            dydt[1] = - 1j*k*self.vf * v_g - Kg*(v_g - v_d) - 1j*k*dP/self.rho_g
-            dydt[2] = - 1j*k*self.rho_d * v_d - 1j*k*self.vf*drho_d
-            dydt[3] = - 1j*k*self.vf * v_d + Kd*(v_g - v_d)
-            dydt[4] = - 1j*k*gammaP0 * v_g - 1j*k*self.vf*dP
+            dydt = np.empty([4], dtype='c8')
+            dydt[0] = - 1j*k*self.rho_g * v_g
+            dydt[2] = - 1j*k*self.rho_d * v_d
+            dydt[1] = - Kg*(v_g - v_d) - 1j*k*drho_g * cs2
+            dydt[3] =   Kd*(v_g - v_d)
+            
             return dydt
 
-        _jac = np.zeros([5,5], dtype='c8')
-        _jac[0,:] = [-1j*k*self.vf, -1j*k*self.rho_g, 0, 0, 0]
-        _jac[1,:] = [0, -Kg - 1j*k*self.vf, 0,  Kg, -1j*k/self.rho_g]
-        _jac[2,:] = [0, 0, -1j*k*self.vf, -1j*k*self.rho_d, 0]
-        _jac[3,:] = [0,  Kd, 0, -Kd - 1j*k*self.vf,      0         ]
-        _jac[4,:] = [0, - 1j*k*gammaP0, 0, 0, -1j*k*self.vf]
+        _jac = np.zeros([4,4], dtype='c8')
+        _jac[0,1] = -1j*k*self.rho_g 
+        _jac[2,3] = -1j*k*self.rho_d
+        _jac[1,:] = [-1j*k * cs2, -Kg, 0,  Kg]
+        _jac[3,:] = [0          ,  Kd, 0, -Kd]
         def jac(t, y,_=None):
             return _jac
 
@@ -92,9 +78,7 @@ class DustyWaveSolver(object):
         # for free by taking the real part).
         e = -1j * self.delta
         IC = np.array([self.rho_g*e, self.cs*e, 
-                       self.rho_d*e, self.cs*e,
-                       gammaP0*e],
-                      dtype='c8')
+                       self.rho_d*e, self.cs*e], dtype='c8')
         integ = ode(f, jac).set_integrator('zvode', method='bdf', 
                                            rtol=1e-12,atol=1e-12,nsteps=10000)
         integ.set_initial_value(IC, 0).set_f_params(None).set_jac_params(None)
@@ -117,11 +101,10 @@ class DustyWaveSolver(object):
         time = sorted(time)
         sol = self._solve_system(list(time))
 
-        def _create_solution(t, drho_g, v_g, drho_d, v_d, dP):
+        def _create_solution(t, drho_g, v_g, drho_d, v_d):
             return _DustyWaveSolution(t, 2*np.pi/self.wavelength,
-                                      self.rho_g, self.rho_d, 
-                                      self.rho_g*self.cs**2/self.GAMMA,
-                                      drho_g, drho_d, v_g, v_d, dP, self.vf)
+                                      self.rho_g, self.rho_d,
+                                      drho_g, drho_d, v_g, v_d)
 
         sol = [_create_solution(t, *s) for t, s in zip(time,sol)]
         if len(sol) == 1:
@@ -134,32 +117,32 @@ if __name__ == "__main__":
     import matplotlib.pyplot as plt
     import matplotlib.animation as animation
     
-    dw = DustyWaveSolver(vf=0, GAMMA=5/3., K=10)
+    dw = DustyWaveSolver()
     
     N = 101
-    times = np.linspace(0, 10, N)
+    times = np.linspace(0, 5, N)
     x     = np.linspace(0, 1, 1001)
     
     sol = dw(times)
 
     f, ax = plt.subplots(2,1)
-    l1, = ax[0].plot(x, sol[0].rho_gas(x)-1,  'k-')     
-    l2, = ax[0].plot(x, sol[0].rho_dust(x)-1, 'k:')
+    l1, = ax[0].plot(x, sol[0].rho_gas(x),  'k-')     
+    l2, = ax[0].plot(x, sol[0].rho_dust(x), 'k:')
     l3, = ax[1].plot(x, sol[0].v_gas(x),  'k-', label='gas') 
     l4, = ax[1].plot(x, sol[0].v_dust(x), 'k:', label='dust')
     plt.legend(frameon=False)
 
 
-    ax[0].set_ylabel(r'$\delta \rho$')
-    #ax[0].set_ylim(-0.002,0.002)
+    ax[0].set_ylabel(r'$\rho$')
+    ax[0].set_ylim(0.99,1.01)
 
     ax[1].set_xlabel(r'$x$')
     ax[1].set_ylabel(r'$v$')
-    #ax[1].set_ylim(-0.001,0.001)
+    ax[1].set_ylim(-0.001,0.001)
 
     def animate(i):
-        l1.set_ydata(sol[i].rho_gas(x)-1)
-        l2.set_ydata(sol[i].rho_dust(x)-1)
+        l1.set_ydata(sol[i].rho_gas(x))
+        l2.set_ydata(sol[i].rho_dust(x))
         l3.set_ydata(sol[i].v_gas(x))
         l4.set_ydata(sol[i].v_dust(x))
         return l1,l2,l3,l4
--- working_backwards/second_order_moving_mesh.py
+++ working_backwards/second_order_moving_mesh.py
@@ -1,8 +1,12 @@
 from __future__ import print_function
 import numpy as np
+from dustywave_sol import *
 
 GAMMA = 5/3.
 NHYDRO = 5
+FB = 0
+K=0.
+
 
 class Arepo2(object):
     """Second-order reconstruction as in AREPO."""
@@ -144,7 +148,7 @@ def max_wave_speed(U):
 
 
 def solve_euler(Npts, IC, reconstruction, tout, Ca = 0.7, fixed_v = 0.0, mesh_type = "fixed", 
-                K=0.1, dust_scheme = "explicit", FB=0):
+                dust_scheme = "explicit"):
     """Test schemes using an Explicit TVD RK integration"""
     # Setup up the grid
     stencil = reconstruction.STENCIL
@@ -165,75 +169,84 @@ def solve_euler(Npts, IC, reconstruction, tout, Ca = 0.7, fixed_v = 0.0, mesh_ty
         Qb[-stencil:] = Qb[stencil:2*stencil]
 
         return Qb
-
-    def update_stage(U, dt, xcin):
-        #1. Apply Boundaries
+    
+    def update_stage_both(xcin, dx, Q, dt):
+        #1. Convert Q -> W
+        U = Q/dx[1:-1].reshape(-1,1)
+        W = cons2prim(U)
+        
+        #2. Apply boundaries
         Ub = boundary(U)
-
-        #2. Compute Primitive variables
-        Wb = cons2prim(Ub)
-
-        #3. Reconstruct the edge states
+        Wb = boundary(W)
+        
+        #3. Compute gradient
         gradW = R.reconstruct(Wb, xcin)
         
-        xe = 0.5*(xc[1:] + xc[:-1])
-        Wm = Wb[1:-1] + gradW*(xe[0:-1] - xc[1:-1]).reshape(-1,1)
-        Wp = Wb[1:-1] + gradW*(xe[1:] - xc[1:-1]).reshape(-1,1)
+        #4. Reconstruct edge states
+        xe = 0.5*(xcin[1:] + xcin[:-1])
+        Wm = Wb[1:-1] + gradW*(xe[0:-1] - xcin[1:-1]).reshape(-1,1)
+        Wp = Wb[1:-1] + gradW*(xe[1:] - xcin[1:-1]).reshape(-1,1)
         WL = Wp[:-1]; WR = Wm[1:]
         
-        #4. Correct velocities
+        #5. Compute velocities
         if mesh_type == "Lagrangian":
+            vc = np.copy(Wb[1:-1,1]_
             vl = WL[:,1]
             vr = WR[:,1]
             vf =  0.5 * (vl+vr)
         elif mesh_type == "fixed":
+            vc = np.full_like(Wb[1:-1,1], fixed_v)
             vf = fixed_v
         
-        #5. Transform lab-frame to face frame.
+        #5a. Transform lab-frame to face frame.
         WL[:,1] -= vf       # subtract face velocity from gas velocity
         WR[:,1] -= vf
         WL[:,4] -= vf
         WR[:,4] -= vf
-        #5. Compute fluxes
-        flux =              HLL_solver(WL, WR, vf)
-
-        #6. Return flux
-        return dt*np.diff(flux, axis=0), gradW
-    
-    def update_stage_prim(W, dWdt, gradW, dt, xcin, vf):
-        #1. Apply Boundaries
-        Wb = boundary(W)
         
-        #Include time correction
+        #6. Compute first fluxes:
+        flux_0 =              HLL_solver(WL, WR, vf)
+        
+        #7. Move the mesh
+        xc, dx = update_mesh(xcin, dt, W)
+        
+        #8. Predict edge states at t+dt
+        #8a. First predict midpoints at t+dt:
+        Wbt = boundary(W)[1:-1]#match gradient extent
+        WLt = np.copy(Wbt[:-1]); WRt = np.copy(Wb[1:])
+        
+        dWdt = time_diff_W(Wb[1:-1], gradW, vc)
         Wb[1:-1] += dWdt*dt
         
-        #2. Compute Conserved variables
-        U = cons2prim(W)
-
-        #3. Reconstruct the edge states
-        #gradW = R.reconstruct(Wb, xcin)
+        #8b. TODO: apply drag forces
         
+        
+        #8c. Reconstruct edge states
         xe = 0.5*(xc[1:] + xc[:-1])
+        
         Wm = Wb[1:-1] + gradW*(xe[0:-1] - xc[1:-1]).reshape(-1,1)
         Wp = Wb[1:-1] + gradW*(xe[1:] - xc[1:-1]).reshape(-1,1)
         WL = Wp[:-1]; WR = Wm[1:]
         
-        WL = Wp[:-1]; WR = Wm[1:]
-        
-        #5. Transform lab-frame to face frame.
+        #Transform lab-frame to face frame.
         WL[:,1] -= vf       # subtract face velocity from gas velocity
         WR[:,1] -= vf 
         WL[:,4] -= vf
         WR[:,4] -= vf
         
-        #4. Compute fluxes
-        flux =                  HLL_solver(Wp[:-1], Wm[1:], vf)
-
-        #5. Update Q
-        return dt*np.diff(flux, axis=0)
+        #9. Compute second flux
+        flux_1 =                  HLL_solver(WL, WR, vf)
+        
+        #10. TODO: compute drag terms
+        
+        #11. Update conserved quantities
+        Q = Q - 0.5*dt*np.diff(flux_0 + flux_1, axis=0)
+        
+        # Return
+        return(xc, dx, Q)
 
     
-    def time_diff_W(W, gradW, vf):# ###, FB):
+    def time_diff_W(W, gradW, vf):# ###):
         W[:,1] -= vf
         W[:,4] -= vf
         dWdt = np.zeros_like(W)
@@ -255,10 +268,10 @@ def solve_euler(Npts, IC, reconstruction, tout, Ca = 0.7, fixed_v = 0.0, mesh_ty
         grad_v_d = gradW[:, 4]
         
         dWdt[:,0] = v_g*grad_rho_g + rho_g*grad_v_g
-        dWdt[:,1] = grad_P/rho_g + v_g*grad_v_g # ###+ FB*( - self.K*rho_d*v_d + self.K*rho_d*v_g)
+        dWdt[:,1] = grad_P/rho_g + v_g*grad_v_g # ###+ FB*( - K*rho_d*v_d + K*rho_d*v_g)
         dWdt[:,2] = GAMMA*P*grad_v_g + v_g * grad_P
         dWdt[:,3] =  v_d*grad_rho_d + rho_d*grad_v_d
-        dWdt[:,4] = v_d*grad_v_d + K*rho_g*v_d - K*rho_g*v_g
+        dWdt[:,4] = v_d*grad_v_d #+ K*rho_g*v_d - K*rho_g*v_g
         dWdt *= -1
         return(dWdt)
     
@@ -284,54 +297,12 @@ def solve_euler(Npts, IC, reconstruction, tout, Ca = 0.7, fixed_v = 0.0, mesh_ty
         dtmax = Ca * min(dx) / max_wave_speed(U)
         dt = min(dtmax, tout-t)
         
-        # 2) Assign face velocities to W
-        # 3) Calculate gradient (NB gradient can be got before / after velocity shift, no problem)
-        # 4.) return flux-updated U
-        F1, gradW1 =                        update_stage(U , dt, xc)
+        # 2) Use Euler solver
+        xc, dx, Q = update_stage_both(xc, dx, Q, dt)
         
-        # 5) Compute predicted dWdt for both sides (requires FACE velocity, so can't do both sides at once)
-        W = cons2prim(U)
-        Wb = boundary(W)[1:-1]#match gradient extent
-        WL = np.copy(Wb[:-1]); WR = np.copy(Wb[1:])
-        
-        # Correct velocities:
-        if mesh_type == "Lagrangian":
-            vl = WL[:,1]
-            vr = WR[:,1]
-            vf =  0.5 * (vl+vr)
-            vc = Wb[:,1]
-        elif mesh_type == "fixed":
-            vf = fixed_v
-            vc = np.full_like(Wb[:,1], fixed_v)
-        
-        dWdt = time_diff_W(Wb, gradW1, vc)
-        dWdtL = dWdt[:-1]
-        dWdtR = dWdt[1:]
-        
-        
-        # 6.) Update mesh
-        xc, dx = update_mesh(xc, dt, W)
-        
-        # 7) Compute fluxes again
-        Fp =                                update_stage_prim(W, dWdt, gradW1, dt, xc, vf)
-        
-        # 8) Time average fluxes (both used dt, so just *0.5)
-        Qold = np.copy(Q)
-        flux = 0.5*(F1+Fp)
-        
-        Q[:,:4] = Qold[:,:4] - flux[:,:4]
-        
-        p_g = Qold[:,2]
-        p_d = Qold[:,4]
-        rho_g = Q[:,0] / dx[1:-1]
-        rho_d = Q[:,3] / dx[1:-1]
-        
-        if dust_scheme == "explicit":
-            Q[:,4] = (p_d - flux[:,4] + K*rho_d*dt*p_g)   /    (1 + K*rho_g*dt)
-            
+        # 3) Update U
         U = Q/dx[1:-1].reshape(-1,1)
         
-    
         t = min(tout, t+dt)
 
     xc = xc[stencil:-stencil]
@@ -341,16 +312,17 @@ def solve_euler(Npts, IC, reconstruction, tout, Ca = 0.7, fixed_v = 0.0, mesh_ty
 
 
 
-def _test_convergence(IC, pmin=4, pmax=10, figs_evol=None, fig_err=None):
+def _test_convergence(IC, pmin=4, pmax=10, figs_evol=None, fig_err=None, t_final=3.0):
     N = 2**np.arange(pmin, pmax+1)
     scheme = Arepo2
-    errs = []
+    errs_gas = []
+    errs_dust = []
     c=None
     label=scheme.__name__
     for Ni in N:
         print (scheme.__name__, Ni)
-        _, W0 = solve_euler(Ni, IC, scheme, 0, Ca = 0.4, mesh_type = "Lagrangian",  fixed_v = 5.0, FB=0)
-        x, W = solve_euler(Ni, IC, scheme, 3.0, Ca = 0.4, mesh_type = "Lagrangian", fixed_v = 5.0, FB=0)
+        _, W0 = solve_euler(Ni, IC, scheme, 0, Ca = 0.4, mesh_type = "Lagrangian",  fixed_v = 5.0)
+        x, W = solve_euler(Ni, IC, scheme, t_final, Ca = 0.4, mesh_type = "Lagrangian", fixed_v = 5.0)
         if figs_evol is not None:
             c = figs_evol[0].plot(x, W[:,0], c=c, 
                                   label=label)[0].get_color()
@@ -363,10 +335,13 @@ def _test_convergence(IC, pmin=4, pmax=10, figs_evol=None, fig_err=None):
             figs_evol[2].set_xlabel('x')
             
             label=None
-
-        errs.append(np.sqrt(np.mean((W[:,1] - W0[:,1])**2)))
+        
+        err = W - IC(x, t=t_final)
+        errs_gas.append(np.sqrt(np.mean((err[:,1])**2)))
+        errs_dust.append(np.sqrt(np.mean((err[:,4])**2)))
     if fig_err is not None:
-        fig_err.loglog(N, errs, c=c, label=scheme.__name__)
+        fig_err.loglog(N, errs_gas, c=c, label=scheme.__name__ + " gas", ls = "-")
+        fig_err.loglog(N, errs_dust, c=c, label=scheme.__name__ + " dust", ls="--")
 
     if fig_err is not None:
         fig_err.set_xlabel('N')
@@ -376,13 +351,32 @@ def _test_convergence(IC, pmin=4, pmax=10, figs_evol=None, fig_err=None):
     if figs_evol is not None:
         figs_evol[0].legend(loc='best',frameon=False)
 
-def init_wave(xc, cs0=1.0, rho0=1.0, v0=1.0, drho=1e-6):
-    kx = 2*np.pi*xc
-
-    W = np.full([len(xc), NHYDRO], np.nan)
-    W[:,0] = rho0 + drho*np.sin(kx)
-    W[:,1] = v0 + drho*cs0*np.sin(kx)
-    W[:,2] = (rho0*cs0**2/GAMMA) * (W[:,0]/rho0)**GAMMA
+def init_wave(xc, cs0=1.0, rho0=1.0, v0=1.0, drho=1e-6, t=0):
+    if t == 0:
+        kx = 2*np.pi*xc
+    
+        W = np.full([len(xc), NHYDRO], np.nan)
+        #gas
+        W[:,0] = rho0 + drho*np.sin(kx)
+        W[:,1] = v0 + drho*cs0*np.sin(kx)
+        W[:,2] = (rho0*cs0**2/GAMMA) * (W[:,0]/rho0)**GAMMA
+    
+        #dust
+        W[:,3] = W[:,0]
+        W[:,4] = W[:,1]
+    
+    else:
+        sol = DustyWaveSolver(K=K, delta=drho, feedback=FB)(t)
+        
+        x = xc - v0*t
+        W = np.full([len(x), NHYDRO], np.nan)
+        
+        W[:,0] = sol.rho_gas(x)
+        W[:,1] = v0 + sol.v_gas(x)
+        W[:,2] = (rho0*cs0**2/GAMMA) * (W[:,0]/rho0)**GAMMA
+        
+        W[:,3] = sol.rho_dust(x)
+        W[:,4] = v0 + sol.v_dust(x)
     return W
 
 
