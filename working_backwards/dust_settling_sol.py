eefe9771b94ad40b2b90683e616b8cb90f301bb7
gouldenm <gouldenm@tcd.ie>
2020-02-11 16:35:57 +0000

    can't even do a fucking ode

--- working_backwards/dust_settling_sol.py
+++ working_backwards/dust_settling_sol.py
@@ -2,7 +2,7 @@ from scipy.integrate import odeint
 import numpy as np
 
 #   Define function that calculates derivatives; returns x' and v' = x''
-
+"""
 def dust_settling_sol(xc, t_final, v_0=0.0, rho_0 = 1.0, K=1.0, g=1.0):
     def dU_dt(U, t):
         return(U[1], -K*rho_0*np.exp(-U[0])*U[1] + g)
@@ -12,6 +12,17 @@ def dust_settling_sol(xc, t_final, v_0=0.0, rho_0 = 1.0, K=1.0, g=1.0):
     U = odeint(dU_dt, U0, ts)
     x = U[:,0]
     v = U[:,1]
+    return(x)
+
+print(dust_settling_sol(0, 1.0))
+"""
+
+def dust_settling_sol(xc, t_final, v_0=0, rho_0 = 1.0, K=1.0, g=1.0):
+    def dv_dt(v, t):
+        return(-K*rho_0*np.exp(-xc)*v + g)
+    
+    ts = np.linspace(0, t_final, 1000)
+    v = odeint(dv_dt, v_0, ts)
     return(v)
 
-print(dust_settling_sol(0, 10.0))
+print(dust_settling_sol(0, 0.5)[-1])
